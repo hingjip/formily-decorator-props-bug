7d9e57dfbd3b18ec1f5112ad74ff4360ef95020b
hingjip <hingjip@hotmail.com>
2021-10-31 16:11:12 +0800

    feat: 重现bug

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import Vue from "vue";
+import VueCompositionAPI from '@vue/composition-api'
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import "ant-design-vue/dist/antd.css";
 
 Vue.config.productionTip = false;
+Vue.use(VueCompositionAPI)
 
 new Vue({
   router,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,55 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <FormProvider :form="form">
+      <SchemaField
+        :schema="{
+          type: 'object',
+          properties: {
+            input: {
+              type: 'string',
+              'x-component': 'Input',
+              'x-component-props': {
+                placeholder: 'test',
+              },
+              'x-decorator': 'FormItem',
+              'x-decorator-props': {
+                label: 'test',
+              },
+            },
+          },
+        }"
+      >
+      </SchemaField>
+    </FormProvider>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import { Input, Form } from "ant-design-vue";
+import { createForm } from "@formily/core";
+import { FormProvider, createSchemaField } from "@formily/vue";
+
+const { SchemaField } = createSchemaField({
+  components: {
+    Input,
+    FormItem: Form.Item,
+  },
+});
 
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
+  components: { FormProvider, SchemaField },
+  data() {
+    return {
+      form: createForm(),
+    };
   },
 };
 </script>
+
+<style scoped>
+.home {
+  width: 800px;
+  margin: 0 auto;
+}
+</style>
